1e33fdec3419725646203465c640121cb25f6f03
shifeixiang <294175702@qq.com>
2017-02-09 10:58:04 +0800

    前端界面更新

---
+++ qq_wb_web/db/db_redis.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+__author__ = 'shifeixiang'
+import redis
+from log.rtx import rtx
+
+def redis_connect():
+    #带密码连接
+    # r = redis.StrictRedis(host='localhost', port=6379, password='npq8pprjxnppn477xssn')
+    try:
+        redis_conn = redis.Redis(host='192.168.15.111',port=6379,db=0)
+    except:
+        rtx('IP','redis连接异常')
+        print "connect redis error"
+        return None
+    return redis_conn
+
+#出wid消息队列
+###redis连接 redis list名字
+def pop_redis_list(redis_conn,redis_list_name):
+    try:
+        qq = redis_conn.lpop(redis_list_name)
+        print "pop ok"
+    except:
+        # redis_conn = redis_connect()
+        print "pop faild"
+        qq = None
+    return qq
+
+# #入临时消息队列
+###redis连接 redis list名字 value
+def push_redis_list_tmp(redis_conn,redis_list_name,value):
+    try:
+        redis_conn.rpush(redis_list_name,value)
+    except:
+        redis_conn = redis_connect()
